cd1aeccea4a8b70795377adbc5f96dc7a8f51a5a
hbyun <hbyun94@gmail.com>
2023-01-01 18:43:09 +0900

    first commit

---
+++ keepajournal/src/main/java/com/jcong/keepajournal/KeepajournalApplication.java
@@ -0,0 +1,13 @@
+package com.jcong.keepajournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepajournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepajournalApplication.class, args);
+	}
+
+}
---
+++ keepajournal/src/test/java/com/jcong/keepajournal/KeepajournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcong.keepajournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepajournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
